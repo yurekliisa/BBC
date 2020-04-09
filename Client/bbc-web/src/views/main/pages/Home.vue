90993a04808177d4266dca78b0b745a22d605fc9
Isa <yurekliisa@gmail.com>
2020-04-09 15:26:19 +0300

    Edited grid system on home page

--- Client/bbc-web/src/views/main/pages/Home.vue
+++ Client/bbc-web/src/views/main/pages/Home.vue
@@ -1,24 +1,31 @@
 <template>
-  <v-layout
-    wrap
-    align-start
-    justify-center
-    row
-    fill-height
-    class="hidden-sm-and-down mb-4"
-  >
-    <v-flex xs3 sm6 md6 lg4 v-for="(item, i) in eventsData" :key="i">
-      <ListCard :item="item" />
+  <v-layout wrap align-start justify-center row fill-height class="mb-4"><!-- >hidden-sm-and-down -->
+    <v-flex md9>
+      <v-row>
+        <v-flex xs3 sm6 md6 v-for="(item, i) in eventsData" :key="i">
+          <ListCard :item="item" />
+        </v-flex>
+      </v-row>
+      <v-row justify="center">
+        <div class="text-center">
+          <v-pagination
+            v-model="page"
+            :length="4"
+            color="deep-purple accent-4"
+            prev-icon="mdi-menu-left"
+            next-icon="mdi-menu-right"
+          ></v-pagination>
+        </div>
+      </v-row>
     </v-flex>
-    <div class="text-center">
-      <v-pagination
-        v-model="page"
-        :length="4"
-        color="deep-purple accent-4"
-        prev-icon="mdi-menu-left"
-        next-icon="mdi-menu-right"
-      ></v-pagination>
-    </div>
+
+    <v-flex md2 offset-md-1>
+      <v-row>
+        <v-flex xs3 sm6 md6 v-for="(item, i) in eventsData" :key="i">
+          <ListCard :item="item" />
+        </v-flex>
+      </v-row>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -29,14 +36,14 @@ import { rotData } from "../../../assets/data/data";
 export default {
   name: "Home",
   components: {
-    ListCard
+    ListCard,
   },
   data() {
     return {
       page: 1,
       showLoader: true,
-      eventsData: rotData
+      eventsData: rotData,
     };
-  }
+  },
 };
 </script>
