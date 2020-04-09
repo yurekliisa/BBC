1c27e750034064a753ef1d58ea94d023986e21d1
Emre <emreoren1905@gmail.com>
2020-04-09 15:22:14 +0300

    TarifAndReceteService Started.

---
+++ Server/BBC_API/src/BBC.Services/Services/TarifAndReceteService/Dto/TarifAndReceteListDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BBC.Services.Services.TarifAndReceteService.Dto
+{
+    public class TarifAndReceteListDto
+    {
+
+    }
+}
---
+++ Server/BBC_API/src/BBC.Services/Services/TarifAndReceteService/TarifAndReceteManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BBC.Services.Services.TarifAndReceteService
+{
+    public class TarifAndReceteManager : ITarifAndReceteService
+    {
+
+    }
+}
