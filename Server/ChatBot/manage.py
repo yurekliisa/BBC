c53afa09d04e5ce8d70dd8189c71b7ad55455f4f
Mahsuni <mahsuni.gnd@gmail.com>
2020-04-13 14:14:36 +0300

    Change wrong file

---
+++ Server/ChatBot/manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tensorflow_Chatbot.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Django çalıştırılamadı. "
+            "Python path yolunu kontrol ediniz."
+        ) from exc
+    execute_from_command_line(sys.argv)
