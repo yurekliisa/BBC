684282f06ffb949a383e1ef1f3e9d2cb28557149
Eylül <aetansoy07@gmail.com>
2020-06-01 01:42:14 +0300

    Added AdminReportController

--- Server/BBC_API/src/BBC.API/Controllers/AdminReportController.cs
+++ Server/BBC_API/src/BBC.API/Controllers/AdminReportController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BBC.API.Helper.Attribute;
+using BBC.Services.Services.AdminService;
+using BBC.Services.Services.AdminService.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +12,22 @@ namespace BBC.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [RequiredAuth]
     public class AdminReportController : ControllerBase
     {
+        private readonly IAdminReportService _adminReportService;
+        public AdminReportController(IAdminReportService adminReportService)
+        {
+            _adminReportService = adminReportService;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(AdminReportWidget), 200)]
+        [Route("AdminReport")]
+        public async Task<IActionResult> AdminReport()
+        {
+            var result = await _adminReportService.Report();
+            return Ok(result);
+        }
+
     }
 }
