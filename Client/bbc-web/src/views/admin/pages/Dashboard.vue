1d3d0ff3c27039943a2b3f5da129dee7bed3e827
yasin <kayhanyasinahmet@gmail.com>
2020-06-07 22:11:17 +0300

    dashboard

--- Client/bbc-web/src/views/admin/pages/Dashboard.vue
+++ Client/bbc-web/src/views/admin/pages/Dashboard.vue
@@ -1,110 +1,29 @@
 <template>
-  <div class="about">
+  <div class="about" v-if="report">
     <h1>Dashboard</h1>
     <v-row>
       <v-col cols="12" md="4">
         <v-card class="mx-auto" color="#26c6da" dark>
           <v-card-title>
-            <v-icon large left>mdi-twitter</v-icon>
-            <span class="title font-weight-light">Twitter</span>
+            <span class="title font-weight-light">{{report.totalTar}}</span>
           </v-card-title>
-
-          <v-card-text
-            class="headline font-weight-bold"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Evan You</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">Toplam Tarif ve Reçete</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
         <v-card class="mx-auto" color="#26c6da" dark>
           <v-card-title>
-            <v-icon large left>mdi-twitter</v-icon>
-            <span class="title font-weight-light">Twitter</span>
+            <span class="title font-weight-light">{{report.totalUser}}</span>
           </v-card-title>
-
-          <v-card-text
-            class="headline font-weight-bold"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Evan You</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">Toplam Kullanıcı Sayısı</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
         <v-card class="mx-auto" color="#26c6da" dark>
           <v-card-title>
-            <v-icon large left>mdi-twitter</v-icon>
-            <span class="title font-weight-light">Twitter</span>
+            <span class="title font-weight-light">{{report.totalComment}}</span>
           </v-card-title>
-
-          <v-card-text
-            class="headline font-weight-bold"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Evan You</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">>Toplam Yorum Sayısı</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -112,90 +31,52 @@
       <v-col cols="12" md="6">
         <v-card class="mx-auto" color="#26c6da" dark>
           <v-card-title>
-            <v-icon large left>mdi-twitter</v-icon>
-            <span class="title font-weight-light">Twitter</span>
+            <span class="title font-weight-light">{{report.totalLobby}}</span>
           </v-card-title>
-
-          <v-card-text
-            class="headline font-weight-bold"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Evan You</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">Toplam Lobi Sayısı</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="6">
         <v-card class="mx-auto" color="#26c6da" dark>
           <v-card-title>
-            <v-icon large left>mdi-twitter</v-icon>
-            <span class="title font-weight-light">Twitter</span>
+            <span class="title font-weight-light">{{report.totalCategory}}</span>
           </v-card-title>
-
-          <v-card-text
-            class="headline font-weight-bold"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Evan You</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">Toplam Kategori Sayısı</v-card-text>
         </v-card>
       </v-col>
     </v-row>
-
-    <v-btn @click.stop="changeFooter()" icon>
-      <v-icon>mdi-application</v-icon>
-    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      report
+    };
+  },
   created() {
     this.$store.commit("SET_panelText", "Dashboard");
   },
   methods: {
-    changeFooter() {
-      this.$store.commit("SET_footer", "yeni footer");
+    fetchData() {
+      axios
+        .get("/AdminReport/AdminReport", {
+          headers: {
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        })
+        .then(result => {
+          console.log(result);
+          if (result.status === 200) {
+            this.recentChefs = result.data;
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
